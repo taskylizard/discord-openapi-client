ab750c1db89af3756f950d9e7f087686a87f90de
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-07-19 21:03:35 +0000

    chore: update hook

--- orval.config.ts
+++ orval.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
     hooks: {
       afterAllFilesWrite:
-        "sed -i 's/formData/createGuildStickerBody/g' generated/generated/default/default.ts",
+        "sed -i 's/formData/createGuildStickerBody/g' ./generated/generated/target.ts",
     },
   },
 });
